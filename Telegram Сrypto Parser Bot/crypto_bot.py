acc7aa32b179a835687e7657fe54ce5a0d5b05aa
Max Shevtsov <maks.nimchenko@gmail.com>
2023-01-20 20:41:55 +0200

    Rename folder
    Add params to requests

--- Telegram Сrypto Parser Bot/crypto_bot.py
+++ Telegram Сrypto Parser Bot/crypto_bot.py
@@ -3,7 +3,8 @@ import telebot
 import requests
 import config_bot
 
-url = 'https://api.binance.com/api/v3/ticker/price?symbol='
+URL_PRICE = 'https://api.binance.com/api/v3/ticker/price'
+URL_HISTORIC_PRICE = 'https://data.binance.vision/?prefix=data/spot/daily/klines/'
 
 bot = telebot.TeleBot(config_bot.TOKEN)
 
@@ -17,12 +18,14 @@ def send_quote(message):
     else:
         count = message.text.split()[0]
         quote = message.text.split()[1]
-    data = requests.get(url + quote.upper() + 'USDT')
+
+    query_params = {'symbol' : quote.upper() + 'USDT'}
+    data = requests.get(URL_PRICE, params=query_params)
     data = data.json()
     try:
         bot.send_message(message.chat.id, f"{count} {quote} = {round(float(count) * float(data['price']), 4)} $")
     except:
-        bot.send_message(message.chat.id, 'Error')
+        bot.send_message(message.chat.id, 'Error. Cryptocurrenc does not exist.')
 
 bot.polling(none_stop=True)
 
